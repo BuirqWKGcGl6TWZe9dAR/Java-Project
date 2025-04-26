6649e60f2986e4bd49b98fefda473e2f1d751e36
IsuruKalharaDeSilva <isurukalhara11@gmail.com>
2025-04-26 12:13:20 +0530

    Java Fix 2

--- src/main/java/cse/school/codejam/Transaction.java
+++ src/main/java/cse/school/codejam/Transaction.java
@@ -44,10 +44,13 @@ public class Transaction {
         
         // Build the message based on the type of transaction
         if (type == TransactionType.DEPOSIT) {
-            msg = "Deposit of Rs " + amount + " to account " + fromAccountNumber;
+            // Deposit: Amount goes to the "to" account (the destination account)
+            msg = "Deposit of Rs " + amount + " to account " + toAccountNumber;
         } else if (type == TransactionType.WITHDRAW) {
-            msg = "Withdrawal of Rs " + amount + " from account " + toAccountNumber;
+            // Withdraw: Amount comes from the "from" account (the source account)
+            msg = "Withdrawal of Rs " + amount + " from account " + fromAccountNumber;
         } else if (type == TransactionType.TRANSFER) {
+            // Transfer: Amount moves from "from" account to "to" account
             msg = "Transfer of Rs " + amount + " from account " + fromAccountNumber + " to account " + toAccountNumber;
         }
 
@@ -58,20 +61,35 @@ public class Transaction {
     // Main method for testing the Transaction class
     public static void main(String[] args) {
         try {
-            // Create a new transaction
+            // Create a new deposit transaction
             Transaction t1 = new Transaction();
             t1.setType(TransactionType.DEPOSIT)
-               .setFromAccountNumber("123456")
+               .setFromAccountNumber("123456")  // From account is relevant for source
+               .setToAccountNumber("789012")    // To account is relevant for destination
                .setAmount(500.00);  // Valid amount
 
             // Print transaction details
-            System.out.println(t1.getTransactionDetails());
+            System.out.println(t1.getTransactionDetails());  // Deposit message
 
-            // Attempting to set an invalid (zero) amount
+            // Create a new withdrawal transaction
             Transaction t2 = new Transaction();
             t2.setType(TransactionType.WITHDRAW)
-               .setToAccountNumber("654321")
-               .setAmount(0);  // This will throw an exception
+               .setFromAccountNumber("789012")  // From account is relevant for source
+               .setToAccountNumber("123456")    // To account (not used in withdrawal)
+               .setAmount(200.00);  // Valid amount
+
+            // Print transaction details
+            System.out.println(t2.getTransactionDetails());  // Withdrawal message
+
+            // Create a new transfer transaction
+            Transaction t3 = new Transaction();
+            t3.setType(TransactionType.TRANSFER)
+               .setFromAccountNumber("123456")  // From account for source
+               .setToAccountNumber("789012")    // To account for destination
+               .setAmount(300.00);  // Valid amount
+
+            // Print transaction details
+            System.out.println(t3.getTransactionDetails());  // Transfer message
 
         } catch (IllegalArgumentException e) {
             // Catch the exception for invalid amount
